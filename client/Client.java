4b6ba6e54849b5274efacccfda6ccb65c6e7f0b1
Hoseini Sara <S.Hoseini@tosan.com>
2023-04-08 18:22:47 +0330

    lots of changes to be able to compile server and client classes

--- client/Client.java
+++ client/Client.java
@@ -1,25 +1,39 @@
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.lang.*;
+import operators.*;
 public class Client { 
+	private static Logger logger = LoggerFactory.getLogger(Client.class.getName());
 	public static void main (String[]args){
 		ServerSocket serverSocket=null;
 		Socket clientSocket= null ; 
 		try {
 			clientSocket = new Socket("localhost", 65534); 
+			logger.info("Connected!");
 			InputStreamReader inputClient = new InputStreamReader(clientSocket.getInputStream());  
             OutputStreamWriter outputClient = new OutputStreamWriter(clientSocket.getOutputStream());  
             BufferedReader bufferedReader = new BufferedReader(inputClient);  
             BufferedWriter bufferedWriter = new BufferedWriter(outputClient); 
 			String Line; 
-			Main objectmainclass = new Main();
-			String Result = objectmainclass.main();
-			bufferedWriter.write(Result);
-			while( (Line= bufferedReader.readLine())!= null){
-				Result= bufferedWriter.write(Line);
+			File file = new File("input"); 
+			File filesList[] = file.listFiles(); //List of all files and directories
+			if (filesList != null || filesList.length != 0) { 
+				for(File myfile : filesList) {
+					(new Thread( new FileCalculator(myfile))).start();
+				}
 			}
+			if (filesList == null || filesList.length == 0) {
+				File myFile = new File("input.txt" );
+				(new Thread( new FileCalculator(myFile))).start(); 	
+			}
+			while( (Line= bufferedReader.readLine())!= null){
+				bufferedWriter.write(Line);
+			}
+			inputClient.close();
+			outputClient.close();
             bufferedReader.close();  
             bufferedWriter.close(); 
 			clientSocket.close(); 
--- server/Server.java
+++ server/Server.java
@@ -1,17 +1,22 @@
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.lang.*;
 import operators.*;
 public class Server {
+	private static Logger logger = LoggerFactory.getLogger(Server.class.getName());
 	public static void main (String[]args){
 		ServerSocket serverSocket = null;
 		Socket clientSocket = null ; 
 		String line;
 		try {
             serverSocket = new ServerSocket( 65534 );
+			logger.info("Server started!");
+			logger.info("Waiting for a client ...");
 			clientSocket= serverSocket.accept();
+			logger.info("Client accepted!");
 			InputStreamReader inputServer = new InputStreamReader(clientSocket.getInputStream());  
             OutputStreamWriter outputServer = new OutputStreamWriter(clientSocket.getOutputStream());  
             BufferedReader bufferedReader = new BufferedReader(inputServer);  
@@ -22,8 +27,13 @@ public class Server {
                 int result;
 				Calculator mycalculator = new Calculator();
 				result = mycalculator.calculating(expression);
-				System.out.println("result:" + result );
+				logger.info("result:" + result );
 			}
+			inputServer.close();
+			outputServer.close();
+			bufferedReader.close();  
+            bufferedWriter.close();
+			logger.info("Closing connection");
 			clientSocket.close();  
 		}			
 		catch( UnsupportedOperatorException	e ){		
